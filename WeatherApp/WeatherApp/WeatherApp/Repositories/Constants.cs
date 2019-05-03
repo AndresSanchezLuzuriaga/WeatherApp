65a6ac795252711097d4e9cf5c14d17bb6218372
Andrés Marcelo Sánchez Luzuriaga <Andres.Sanchez.Luzuriaga@tajamar365.com>
2019-05-03 17:20:08 +0200

    Creación del proyecto WeatherApp

---
+++ WeatherApp/WeatherApp/WeatherApp/Repositories/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp
+{
+    public static class Constants
+    {
+        public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        //public const string OpenWeatherMapAPIKey = "INSERT_API_KEY_HERE";
+        public const string OpenWeatherMapAPIKey = "81dbb02a439114a3128632faa0a71620";
+    }
+}
